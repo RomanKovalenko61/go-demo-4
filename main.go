b6e161a4141aca2503324d5b4b2f75c0bb3801ef
Roman <astek14@mail.ru>
2025-04-11 20:52:28 +0300

    9.3 create instance

--- main.go
+++ main.go
@@ -2,23 +2,39 @@ package main
 
 import "fmt"
 
+type account struct {
+	login string
+	password string
+	url string
+}
+
 func main() {
-	a := [4]int{1, 2, 3, 4}
-	b := [4]int{10, 20, 30, 40}
-	reverse(&a) // Меняет порядок элементов на обратный
-	reverse0(&b)
-	fmt.Println(a)
-	fmt.Println(b)
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	/*account1 := account{
+		login,
+		password,
+		"",
+	}*/
+
+	account1 := account{
+		password: password,
+		url: url,
+		login: login,
+	}
+
+	outputPassword(login, password, url)
 }
 
-func reverse(arr *[4]int) {
-	for index, value := range *arr {
-		(*arr)[len(arr) - 1 - index] = value
-	}
+func promptData(prompt string) string{
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
 }
 
-func reverse0(arr *[4]int) {
-	for i, j := 0, len(*arr) - 1; i < j; i, j = i + 1, j - 1 {
-    	arr[i], arr[j] = arr[j], arr[i]
-	}
+func outputPassword(login, password, url string) {
+	fmt.Println(login, password, url)
 }
