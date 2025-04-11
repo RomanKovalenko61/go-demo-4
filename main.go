cee68f5a599b1d5ed759489570f34ef190559030
Roman <astek14@mail.ru>
2025-04-11 18:38:16 +0300

    8.3 Pointer

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("password")
+	a := 5
+	pointerA := &a
+	res := double(a)
+	fmt.Println(pointerA)
+	fmt.Println(res)
+}
+
+func double(num int) int {
+	return num * 2
 }
